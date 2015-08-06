2f0d58daa05a04cab5a708ece06122f176146fd2
Jay Are Galinada <jayaregalinada@gmail.com>
2015-08-06 11:57:58 +0800

    Create ProgressBarTrait for ProgressBar instance

---
+++ src/ProgressBarTrait.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace JeroenG\Packager;
+
+use Symfony\Component\Console\Helper\ProgressBar;
+
+trait ProgressBarTrait {
+
+    private function createProgressBar($max = 0)
+    {
+        if(version_compare($this->laravel->version(), '5.1', '>=')) {
+            return $this->output->createProgressBar($max);
+        } else {
+            return new ProgressBar($this->output, $max);
+        }
+    }
+
+}
