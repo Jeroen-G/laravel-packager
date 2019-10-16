8a19e7226007f0da2389ac286f6f91df9093a5a8
Kerstens Maxim <maxim.kerstens@gmail.com>
2019-10-16 07:19:44 +0200

    added git info when adding --git or -g option to packager:list

--- src/Commands/ListPackages.php
+++ src/Commands/ListPackages.php
@@ -3,6 +3,8 @@
 namespace JeroenG\Packager\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+use Symfony\Component\Process\Process;
 
 /**
  * List all locally installed packages.
@@ -15,7 +17,7 @@ class ListPackages extends Command
      * The name and signature of the console command.
      * @var string
      */
-    protected $signature = 'packager:list';
+    protected $signature = 'packager:list {--g|git}';
 
     /**
      * The console command description.
@@ -36,13 +38,67 @@ class ListPackages extends Command
         $packages = [];
         foreach ($repositories as $name => $info) {
             $path = $info['url'];
-            $pattern = '{'.addslashes($packages_path).'(.*)$}';
+            $pattern = '{' . addslashes($packages_path) . '(.*)$}';
             if (preg_match($pattern, $path, $match)) {
                 $packages[] = explode(DIRECTORY_SEPARATOR, $match[1]);
             }
         }
 
+        if ($this->option('git')) {
+            $this->renderGitPackages($packages, $packages_path);
+
+            return;
+        }
+
         $headers = ['Package', 'Path'];
         $this->table($headers, $packages);
+    }
+
+    protected function renderGitPackages($packages, $packages_path)
+    {
+        $gitPackages = collect($packages)
+            ->map(function ($package) use ($packages_path) {
+                return [
+                    'vendor' => $package[0],
+                    'name'   => $package[1],
+                    'path'   => $packages_path . implode(DIRECTORY_SEPARATOR, [$package[0], $package[1]]),
+                ];
+            })
+            ->filter(function ($package) {
+                return file_exists($package['path'] . DIRECTORY_SEPARATOR . '.git');
+            })
+            ->map(function ($package) {
+                // Always run fetch first
+                (new Process('git fetch', $package['path']))->disableOutput()->run();
+
+                // get the amount of commits difference
+                $commitDifference = 0;
+                (new Process('git rev-list HEAD..origin --count', $package['path']))
+                    ->run(function ($type, $buffer) use (&$commitDifference) {
+                        $commitDifference = str_replace(["\n", "\r"], '', $buffer);
+                    });
+
+                // Get the current branch
+                $branch = null;
+                (new Process('git branch', $package['path']))
+                    ->run(function ($type, $buffer) use (&$branch) {
+                        if (Str::startsWith($buffer, '*')) {
+                            $branch = str_replace(["\n", "\r", ' ', '*'], '', $buffer);
+                        }
+
+                    });
+
+                return [
+                    $package['vendor'],
+                    $package['name'],
+                    $commitDifference,
+                    $branch,
+                ];
+            });
+
+
+        $headers = ['Package', 'Path', 'Commits behind', 'Branch'];
+
+        $this->table($headers, $gitPackages->toArray());
     }
 }
