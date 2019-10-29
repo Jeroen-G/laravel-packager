bab7f5759247e9d4ee1fc351741de7725afc09d5
Zlatoslav Desyatnikov <zdesyatnikov@b2broker.net>
2019-10-29 18:38:39 +0300

    Get CURL_VERIFY param from config() instead of env()

--- config/packager.php
+++ config/packager.php
@@ -8,6 +8,8 @@ return [
      */
     'skeleton' => 'http://github.com/Jeroen-G/packager-skeleton/archive/master.zip',
 
+    'curl_verify_cert' => env('CURL_VERIFY', true),
+
     /*
      * You can set defaults for the following placeholders.
      */
